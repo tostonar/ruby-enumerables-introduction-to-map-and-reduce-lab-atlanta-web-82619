7f4c18980bbe1de9ee914245c61004141af52004
Andrea Toston <tostonar@gmail.com>
2019-07-24 16:24:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,12 +56,13 @@ def reduce_to_all_true(array)
 end
 
 def reduce_to_any_true(array)
-  i = 0
-  while i < array.length do
-    if array[i]
-      return true
-    end
-    i += 1
-  end
-  return false
+  # i = 0
+  # while i < array.length do
+  #   if array[i]
+  #     return true
+  #   end
+  #   i += 1
+  # end
+  # return false
+  array.any?
 end
