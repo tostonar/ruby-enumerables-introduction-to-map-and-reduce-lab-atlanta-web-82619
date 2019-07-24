a90900d7d765ea099d54e0f3b724474c47cb5555
Andrea Toston <tostonar@gmail.com>
2019-07-24 16:05:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,11 +4,21 @@ def map_to_negativize(array)
   i = 0
   while i < array.length do
     result.push(array[i] * -1 )
-    i+=1
+    i += 1
   end
   result
 end
 
 def map_to_no_change(array)
   array
+end
+
+def map_to_double(arr)
+  result = []
+  i = 0
+  while i < array.length do
+    result.push(array[i] * 2 )
+    i += 1
+  end
+  result
 end
